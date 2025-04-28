924114d23cb0559ad5dd8d28d68004677f2b49e1
gillesF78 <gilles.foucault@gmail.com>
2025-04-28 16:59:44 +0200

    Update nanobleimu.py
    Added:
    - excel workbook file output...
    - time changed to float32 in milliseconds
    - data changed from dict to numpy array of 7 float32
    - displays of the acquisition rate

--- nanobleimu.py
+++ nanobleimu.py
@@ -1,19 +1,17 @@
-# -*- coding: utf-8 -*-
 """
-A simple program to read IMU data at 100Hz from the Arduino Nano 33 IOT board.
+A simple program to read IMU data at approx.50Hz from the Arduino Nano 33 IOT board.
 """
 
 import asyncio
-from multiprocessing import ProcessError
-import struct
 import sys
 from typing import Dict
-from importlib_metadata import csv
-import keyboard
+from datetime import datetime
+import numpy as np
+import pandas as pd
 
-import bleak
 from bleak import BleakClient
 from bleak import discover
+from bleak import BleakScanner
 
 
 # These values have been randomly generated - they must match between the Central and Peripheral devices
@@ -31,19 +29,21 @@ class NanoIMUBLEClient(object):
         self._running = False
         self._uuid = uuid
         self._found = False
-        self._data = {"time": 0, 
-                      "ax": 0.0, "ay": 0.0, "az": 0.0,
-                      "gx": 0.0, "gy": 0.0, "gz": 0.0}
+        self._data = np.zeros(7, dtype=np.float32)
         self._csvout = csvout 
         self.newdata = False
+        self.debug = False
+        self._records = np.array([])#np.zeros(7, dtype=np.float32)
         self.printdata = True
+        self.starttime = datetime.now().strftime("_%m%d%Y_%H%M%S")
+        self.csvfile = open(f"out{self.starttime}.csv", 'w', newline='')
     
     @property
     def connected(self) -> bool:
         return self._connected
     
     @property
-    def data(self) -> Dict:
+    def data(self):
         return self._data
 
     @property
@@ -64,29 +64,35 @@ class NanoIMUBLEClient(object):
         
         # Currently not connected.
         print('Arduino Nano IMU Service')
-        print('Looking for Peripheral Device...')
-        devices = await discover()
-        for d in devices:
-            if 'Arduino Nano 33 BLE Sense'in d.name:
-                self._found = True
-                self._device = d
-                sys.stdout.write(f'Found Peripheral Device {self._device.address}. ')
-                break
+        print("- Discovering peripheral device...")
+        device = await BleakScanner.find_device_by_filter(lambda d, ad: d.name == "Arduino Nano 33 BLE", timeout=1)
         
-        # Connect and do stuff.
-        async with BleakClient(d.address) as self._client:
-            sys.stdout.write(f'Connected.\n')
-            self._connected = True
-            # Start getting data.
-            await self.start()
-            # Run while connected.
-            while self._connected:
-                if self._running:
-                    # Print data.
-                    if self.printdata and self.newdata:
-                        self.print_newdata()
-                        self.newdata = False
+        if device:
+            print("* Peripheral device found!")
+            print(f"* Device address: {device.address}")
+            print(f"* Device name: {device.name}")
+            self._found = True
+            self._device = device
+            # Connect and do stuff.
+            async with BleakClient(device.address) as self._client:
+                sys.stdout.write(f'Connected.\n')
+                self._connected = True
+                # Start getting data.
+                await self.start()
+                # Run while connected.
+                while self._connected:
+                    if self._running:
+                        # Print data.
+                        if(self.debug): print(f"self.newdata: {self.newdata}")
+                        if self.printdata and self.newdata:
+                            self.print_newdata()
+                            self.record_newdata()
+                            self.newdata = False
+                    else:
+                        print("Not running")
                     await asyncio.sleep(0)
+        else:
+            print("* No peripheral device found!")
     
     async def disconnect(self) -> None:
         if self._connected:
@@ -99,8 +105,10 @@ class NanoIMUBLEClient(object):
     async def start(self) -> None:
         if self._connected:
             # Start notification
+            if(self.debug): print("BLE: start notify")
             await self._client.start_notify(self._uuid, self.newdata_hndlr)
             self._running = True
+            if(self.debug): print("BLE: running")
     
     async def stop(self) -> None:
         if self._running:
@@ -108,41 +116,69 @@ class NanoIMUBLEClient(object):
             await self._client.stop_notify(self._uuid)
         
     def newdata_hndlr(self, sender, data):
-        self._data['time'] = struct.unpack('<L', bytes(data[0:4]))[-1]
-        self._data['ax'] = struct.unpack('<f', bytes(data[4:8]))[-1]
-        self._data['ay'] = struct.unpack('<f', bytes(data[8:12]))[-1]
-        self._data['az'] = struct.unpack('<f', bytes(data[12:16]))[-1]
-        self._data['gx'] = struct.unpack('<f', bytes(data[16:20]))[-1]
-        self._data['gy'] = struct.unpack('<f', bytes(data[20:24]))[-1]
-        self._data['gz'] = struct.unpack('<f', bytes(data[24:28]))[-1]
+        self._data[:] = np.frombuffer(data, dtype=np.float32, count=7)
         self.newdata = True
+        if(self.debug): print(f"receiving newdata: {self._data[0]}")
+    
+    def record_newdata(self) -> None:
+        sample_count = int(self._records.size/self._data.size);
+        freq = 0
+        if(self._records.size):
+            self._data[0] = (self._data[0] - self._start_recording_time) * 1E-3
+            self._records = np.vstack((self._records, self._data))
+            freq = sample_count/self._data[0]
+        else:
+            self._records = self._data
+            self._start_recording_time = self._data[0]
+            self._data[0] = 0
+        sys.stdout.write(f"\rsample count: {sample_count}\tdata rate: {round(freq)} Hz")
+        sys.stdout.flush()
     
     def print_newdata(self) -> None:
         if self._csvout:
-            _str = (f"{self.data['time']/1000000.0:+3.3f}, " +
-                    f"{self.data['ax']:+1.3f}, " + 
-                    f"{self.data['ay']:+1.3f}, " + 
-                    f"{self.data['az']:+1.3f}, " +
-                    f"{self.data['gx']:+3.3f}, " +
-                    f"{self.data['gy']:+3.3f}, " +
-                    f"{self.data['gz']:+3.3f}\n")
+            _str = (f"{self.data[0]*0.001:+3.3f}, " +
+                    f"{self.data[1]:+1.3f}, " + 
+                    f"{self.data[2]:+1.3f}, " + 
+                    f"{self.data[3]:+1.3f}, " +
+                    f"{self.data[4]:+3.3f}, " +
+                    f"{self.data[5]:+3.3f}, " +
+                    f"{self.data[6]:+3.3f}\n")
+            self.csvfile.write(_str)
         else:
-            _str = (f"\r Time: {self.data['time']/1000000.0:+3.3f} | " +
+            _str = (f"\r Time: {self.data[0]*0.001:+3.3f} | " +
                     "Accl: " +
-                    f"{self.data['ax']:+1.3f}, " + 
-                    f"{self.data['ay']:+1.3f}, " + 
-                    f"{self.data['az']:+1.3f} | " +
+                    f"{self.data[1]:+1.3f}, " + 
+                    f"{self.data[2]:+1.3f}, " + 
+                    f"{self.data[3]:+1.3f} | " +
                     "Gyro: " +
-                    f"{self.data['gx']:+3.3f}, " +
-                    f"{self.data['gy']:+3.3f}, " +
-                    f"{self.data['gz']:+3.3f}")
-        sys.stdout.write(_str)
-        sys.stdout.flush()
+                    f"{self.data[4]:+3.3f}, " +
+                    f"{self.data[5]:+3.3f}, " +
+                    f"{self.data[6]:+3.3f}")
+            sys.stdout.write(_str)
+            sys.stdout.flush()
+            
+    def write_records_to_csv(self) -> None:
+        starttime = datetime.now().strftime("_%m%d%Y_%H%M%S")
+        filename = f"out{starttime}.csv"
+        print(f"Write records to file {filename}")
+        np.savetxt(filename, self._records, delimiter=',', fmt='%3.3f',
+                   header="time,ax,ay,az,gx,gy,gz", comments='')
+        
+    def write_records_to_excel(self) -> None:
+        starttime = datetime.now().strftime("_%m%d%Y_%H%M%S")
+        filename = f"out{starttime}.xlsx"
+        print(f"Write records to file {filename}")
+        #df = pd.DataFrame.from_records(self._records)
+        print(self._records)
+        df = pd.DataFrame(self._records, columns=['time','ax','ay','az','gx','gy','gz'])
+        with pd.ExcelWriter(filename, engine='xlsxwriter') as writer:
+            df.to_excel(writer)
+        
 
-
+imu_client = NanoIMUBLEClient(uuid=IMU_UUID, csvout=True)
 async def run():
     # Create a new IMU client.
-    imu_client = NanoIMUBLEClient(IMU_UUID, False)
+    global imu_client
     await imu_client.connect()
 
 
@@ -154,5 +190,7 @@ if __name__ == "__main__":
         print("sdgdsag")
     except KeyboardInterrupt:
         print('\nReceived Keyboard Interrupt')
+        imu_client.write_records_to_excel()
     finally:
         print('Program finished')
+
